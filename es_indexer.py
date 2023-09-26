12632672a935d9afbb821d6cf66122b3cb3365ba
Damien <damien.laqueuvre@gmail.com>
2023-09-26 16:13:55 +0200

    mapping indexing time

--- es_indexer.py
+++ es_indexer.py
@@ -10,37 +10,42 @@ client = Elasticsearch(
 #Test de réponse
 #print(client.info())
 
-with open("mapping.json","r") as file :
-    mapping = json.load(file)
 
 
-#Reset index (ingnoré si l'index n'existe pas)
-client.options(ignore_status=[400,404]).indices.delete(index='birds')
+def index_bird(client, nom_mapping, nom_index) : 
+    with open(nom_mapping+".json","r") as file :
+        mapping = json.load(file)
 
-#Create index
-response = client.indices.create(
-    index="birds",
-    body=mapping,
-    ignore=400
-)
+    #Reset index (ingnoré si l'index n'existe pas)
+    client.options(ignore_status=[400,404]).indices.delete(index=nom_index)
+
+    #Create index
+    response = client.indices.create(
+        index=nom_index,
+        body=mapping,
+        ignore=400
+    )
 
 
-if 'acknowledged' in response:
-    if response['acknowledged'] == True:
-        print ("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
+    if 'acknowledged' in response:
+        if response['acknowledged'] == True:
+            print ("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
 
-# catch API error response
-elif 'error' in response:
-    print ("ERROR:", response['error']['root_cause'])
-    print ("TYPE:", response['error']['type'])
+    # catch API error response
+    elif 'error' in response:
+        print ("ERROR:", response['error']['root_cause'])
+        print ("TYPE:", response['error']['type'])
 
-start_time = time.time()
+    start_time = time.time()
 
-es_file_crawler.folder_crawl("/home/damidas/Bureau/Cours/Stage/Bird/bird/bird", client, "birds")
+    es_file_crawler.folder_crawl("/home/damidas/Bureau/Cours/Stage/Bird/bird/bird", client, nom_index)
 
-# Arrêtez le chronomètre
-end_time = time.time()
-# Calculez la durée d'exécution
-execution_time = end_time - start_time
+    # Arrêtez le chronomètre
+    end_time = time.time()
+    # Calculez la durée d'exécution
+    execution_time = end_time - start_time
 
-print(f"Le temps d'exécution était de {execution_time} secondes.")
+    print(f"Le temps d'exécution pour {nom_mapping} était de {execution_time} secondes.")
+
+index_bird(client, "mapping", "birds")
+index_bird(client, "mapping-analyzer", "perf-birds")
