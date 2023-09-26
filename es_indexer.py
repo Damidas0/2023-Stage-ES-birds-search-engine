2c7e29c66c8b3671e8fd4c41969ff5fec208538c
Damien <damien.laqueuvre@gmail.com>
2023-09-26 09:34:50 +0200

    refactor mapping into json file

--- es_indexer.py
+++ es_indexer.py
@@ -1,3 +1,4 @@
+import json
 from elasticsearch import Elasticsearch
 import es_file_crawler
 
@@ -8,55 +9,15 @@ client = Elasticsearch(
 #Test de réponse
 #print(client.info())
 
-mapping = {
-    "settings": {
-        "number_of_shards": 1
-    },
-    "mappings": {
-        "properties": {
-            "nom_commun": {"type": "text"},
-            "nom_latin": {"type": "text"}, 
-            "ordre": {"type": "text"},
-            "famille": {"type": "text"},
-            "genre": {"type": "text"},
-            "espece": {"type": "text"},
-            "descriteur_nom": {"type": "text"},
-            "descripteur_annee": {"type": "integer"},
+mapping = json.load("mapping.json")
 
-            "status_conservation": {"type": "keyword"},
-
-            "taille_cm": {"type": "integer"},
-            "envergure_min": {"type": "integer"},
-            "envergure_max": {"type": "integer"},
-
-            "poids_min": {"type": "integer"}, 
-            "poids_max": {"type": "integer"}, 
-
-            "longevite": {"type": "integer"},
-
-            "nom_etranger_liste": {"type": "text"},
-
-            #Paragraphes
-            "description_espece": {"type": "text"},
-            "chant": {"type": "text"},
-            "habitat": {"type": "text"},
-            "comportement": {"type": "text"},
-            "vol": {"type": "text"},
-            "regime_alimentaire": {"type": "text"},
-            "reproduction": {"type": "text"},
-            "distribution": {"type": "text"},
-            "protection": {"type": "text"}
-        }
-        
-    }
-}
 
 #Reset index (ingnoré si l'index n'existe pas)
-client.options(ignore_status=[400,404]).indices.delete(index='test-index')
+client.options(ignore_status=[400,404]).indices.delete(index='perf-birds')
 
 #Create index
 response = client.indices.create(
-    index="birds",
+    index="perf-birds",
     body=mapping,
     ignore=400
 )
@@ -72,6 +33,6 @@ elif 'error' in response:
     print ("TYPE:", response['error']['type'])
 
 
-es_file_crawler.folder_crawl("/home/damidas/Bureau/Cours/Stage/Bird/bird/bird", client, "birds")
+es_file_crawler.folder_crawl("/home/damidas/Bureau/Cours/Stage/Bird/bird/bird", client, "perf-birds")
 
 
